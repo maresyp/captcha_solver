34d438b8579b1c66bcc61a7cf0ef328e602a466f
maresyp <mattq.2000@icloud.com>
2021-08-26 17:04:50 +0200

    add __init__, __str__ methods

--- image_recognition/template_matching.py
+++ image_recognition/template_matching.py
@@ -3,5 +3,14 @@ import cv2
 
 
 class TemplateMatcher:
-    def __init__(self, haystack, needle):
-        pass
+    def __init__(self, haystack: str, needle: str):
+        """ Load files """
+        self.haystack = cv2.imread(haystack, cv2.IMREAD_UNCHANGED)
+        if self.haystack is None:
+            raise FileNotFoundError(f"File '{haystack}' does not exist")
+        self.needle = cv2.imread(needle, cv2.IMREAD_UNCHANGED)
+        if self.needle is None:
+            raise FileNotFoundError(f"File '{needle}' does not exist")
+
+    def __str__(self):
+        return f'{self.__class__.__name__} for {self.needle} in {self.haystack}'

dd9ec9d67f9e6d56701270cfb49122a67b6a33e9
maresyp <mattq.2000@icloud.com>
2021-09-01 13:16:46 +0200

    add Matcher base class

---
+++ image_recognition/matcher.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+
+
+class Matcher:
+    """ Base class for Matchers """
+
+    def __init__(self, haystack: np.ndarray):
+        self.haystack = haystack
+
+    def __str__(self):
+        return f'{self.__class__.__name__} for {self.haystack}'
+
+    def draw_rectangle(self, rectangle: list[int, int, int, int]) -> None:
+        """ Draw rectangle """
+        for x, y, width, height in rectangle:
+            cv.rectangle(self.haystack, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+    def show(self) -> None:
+        """ Show haystack and wait for key """
+        cv.imshow('Matches', self.haystack)
+        cv.waitKey()

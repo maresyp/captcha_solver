17c00425bee1ceb20ddc78f5d737502a34467138
maresyp <mattq.2000@icloud.com>
2021-09-01 20:10:34 +0200

    add get_center_point, draw_cross, change draw_rectangle

--- image_recognition/matcher.py
+++ image_recognition/matcher.py
@@ -11,10 +11,20 @@ class Matcher:
     def __str__(self):
         return f'{self.__class__.__name__} for {self.haystack}'
 
-    def draw_rectangle(self, rectangle: list[int, int, int, int]) -> None:
+    def draw_rectangle(self, rectangle: list[int, int, int, int]):
         """ Draw rectangle """
         x, y, width, height = rectangle
         cv.rectangle(self.haystack, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+    @staticmethod
+    def get_center_point(rectangle: list[int, int, int, int]) -> tuple[int, int]:
+        """ Calculate center point of rectangle """
+        x, y, width, height = rectangle
+        return x + width // 2, y + height // 2
+
+    def draw_cross(self, point: tuple[int, int], color=(0, 255, 0)):
+        """ Draw x at center point of rectangle """
+        cv.drawMarker(self.haystack, (point[0], point[1]), color, cv.MARKER_CROSS)
 
     def show(self) -> None:
         """ Show haystack and wait for key """

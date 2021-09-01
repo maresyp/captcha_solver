9f1afae68c86f9f62c7283b9eb9cfa55d04611e8
maresyp <mattq.2000@icloud.com>
2021-09-01 13:29:45 +0200

    rewrite

--- image_recognition/template_matching.py
+++ image_recognition/template_matching.py
@@ -1,44 +1,46 @@
 from typing import Optional
-import cv2
+import cv2 as cv
 import numpy as np
+from matcher import Matcher
 
 
-class TemplateMatcher:
-    __MATCHING_METHOD = cv2.TM_CCOEFF_NORMED
-
-    def __init__(self, haystack: np.ndarray):
-        self.haystack = haystack
-
-    def __str__(self):
-        return f'{self.__class__.__name__} for {self.haystack}'
+class TemplateMatcher(Matcher):
+    __MATCHING_METHOD = cv.TM_CCOEFF_NORMED
 
     def match_one(self, needle: np.ndarray, threshold: float) -> Optional[tuple[float, tuple[int, int]]]:
         """ Try to find matching needle in haystack """
 
-        _, max_val, _, max_loc = cv2.minMaxLoc(
-            cv2.matchTemplate(self.haystack, needle, self.__MATCHING_METHOD)
+        _, max_val, _, max_loc = cv.minMaxLoc(
+            cv.matchTemplate(self.haystack, needle, self.__MATCHING_METHOD)
         )
         return max_val, max_loc if max_val >= threshold else None
 
-    def match_multiple(self, needle: str, amount: int, threshold: float) -> \
-            Optional[list[tuple[float, tuple[int, int]]]]:
-        return None
-
-    def draw_rectangle(self, loc: tuple[int, int], size: tuple[int, int]) -> None:
+    def match_multiple(self, needle: np.ndarray, threshold: float) -> Optional[np.ndarray]:
         """
-        TODO: fix this docstring
-        Draw rectangle of needle size(x, y) at given max_loc
-        If size is not specified use last used size of needle
+        Search for needles in haystack \n
+        returns array of rectangles around found objects
+        :return: np.ndarray[np.ndarray[int, int, int, int]]
         """
-        cpy = self.haystack.copy()
-        cv2.rectangle(cpy, loc, (loc[0] + size[0], loc[1] + size[1]), (0, 255, 255), 2)
-        cv2.imshow('Match', cpy)
-        cv2.waitKey()
+        match_result: np.ndarray = cv.matchTemplate(self.haystack, needle, self.__MATCHING_METHOD)
+        locations: list[tuple] = list(zip(*np.where(match_result >= threshold)[::-1]))
+        # rect -> [x, y, width, height]
+        rectangles = []
+        for loc in locations:
+            rect = [int(loc[0]), int(loc[1]), needle.shape[1], needle.shape[0]]
+            rectangles.append(rect)
+            rectangles.append(rect)
+        rectangles, _ = cv.groupRectangles(rectangles, 1, 0.5)
+        return rectangles if len(rectangles) else None
 
 
 if __name__ == '__main__':
     from utils import read_image
 
-    tm = TemplateMatcher(read_image('../test_files/hard/haystack.jpg', cv2.IMREAD_GRAYSCALE))
-    x = tm.match_one(read_image('../test_files/hard/needle_1.jpg', cv2.IMREAD_GRAYSCALE), .1)
-    tm.draw_rectangle(x[1], (50, 50))
+    tm = TemplateMatcher(read_image('../test_files/easy/sample_multiple/haystack.jpg'))
+    x = tm.match_multiple(read_image('../test_files/easy/sample_multiple/needle.jpg'), .5)
+    print(type(x))
+    for re in x:
+        print(re)
+        tm.draw_rectangle(re)
+    tm.show()
+# tm.draw_rectangle(x[1], (50, 50))

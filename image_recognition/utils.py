8f577567b889399180e229e2dce5d100a5fce092
maresyp <mattq.2000@icloud.com>
2021-09-13 17:44:30 +0200

    move read_image

--- image_recognition/utils.py
+++ image_recognition/utils.py
@@ -1,10 +1,2 @@
-import cv2
+import cv2 as cv  # type: ignore
 import numpy as np
-
-
-def read_image(filename: str, method=cv2.IMREAD_UNCHANGED) -> np.ndarray:
-    """ Wrapper for cv2.imread() that raises exception when file is not loaded properly """
-    img = cv2.imread(filename, method)
-    if img is None:
-        raise FileNotFoundError(f'{filename} not found')
-    return img
